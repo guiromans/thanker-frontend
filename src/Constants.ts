fe2a2c580beff291bc16f527540c3c5eabc0c0cb
Guilherme Amoroso Romão <guilherme.romao@mirriad.com>
2024-05-26 15:46:47 +0100

    Connecting via HTTP for now

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,4 @@
-const PORT: number = 8095;
-const LOCAL_URL: string = `http://localhost:${PORT}`;
-const CLOUD_URL: string = `https://ec2-54-195-42-42.eu-west-1.compute.amazonaws.com:${PORT}`;
+const LOCAL_URL: string = `http://localhost:8095`;
+const CLOUD_URL: string = `http://ec2-54-195-42-42.eu-west-1.compute.amazonaws.com:8095`;
 
 export const SERVICE_URL = CLOUD_URL;
