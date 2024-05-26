7d7c7c868720f17e3d8f4221d3f74dc4ffaf49f0
Guilherme Amoroso Romão <guilherme.romao@mirriad.com>
2024-05-26 01:44:45 +0100

    Added port constant

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,5 @@
-const LOCAL_URL: string = "http://localhost:8095";
-const CLOUD_URL: string = "https://ec2-54-195-42-42.eu-west-1.compute.amazonaws.com:8095";
+const PORT: number = 8095;
+const LOCAL_URL: string = `http://localhost:${PORT}`;
+const CLOUD_URL: string = `https://ec2-54-195-42-42.eu-west-1.compute.amazonaws.com:${PORT}`;
 
 export const SERVICE_URL = CLOUD_URL;
