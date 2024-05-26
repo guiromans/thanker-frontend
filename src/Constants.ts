a96cf6a69407c790e7df0978cbd9a0256880ae2a
Guilherme Amoroso Romão <guilherme.romao@mirriad.com>
2024-05-27 00:23:57 +0100

    Updated port

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
 const LOCAL_URL: string = `http://localhost:8095`;
-const CLOUD_URL: string = `http://ec2-54-195-42-42.eu-west-1.compute.amazonaws.com:8095`;
+const CLOUD_URL: string = `https://ec2-54-195-42-42.eu-west-1.compute.amazonaws.com:8443`;
 
 export const SERVICE_URL = CLOUD_URL;
