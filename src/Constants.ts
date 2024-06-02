a347fbbc27614a6ba35e5df3617036a912fc2255
Guilherme Amoroso Romão <guilherme.romao@mirriad.com>
2024-06-02 22:21:25 +0100

    Fixed URL

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
-const LOCAL_URL: string = `http://localhost:8095`;
+const LOCAL_URL: string = `https://localhost:8443`;
 const CLOUD_URL: string = `https://api.thanker.co:8443`;
 
 export const SERVICE_URL = CLOUD_URL;
