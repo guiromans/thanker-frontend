300c37d8beeb9617d8ffb5b5f2b2180b2278ebf0
Guilherme Amoroso Romão <guilherme.romao@mirriad.com>
2024-06-03 22:34:32 +0100

    Removed basename

--- src/App.tsx
+++ src/App.tsx
@@ -152,7 +152,7 @@ const App = () => {
           userId={userId}
         />
         <React.StrictMode>
-        <BrowserRouter basename="https://thanker.co">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={hasValidToken() ? <UserPage userId={userId} language={language} loadingUsers={loadingUsers}/> : <LoginPage onLogged={handleLogged} /> } />
             <Route path="/login" element={!hasValidToken() ? <LoginPage onLogged={handleLogged}/> : <UserPage userId={userId} language={language}  loadingUsers={loadingUsers}/>} />
