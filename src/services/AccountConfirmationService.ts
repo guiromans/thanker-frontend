e86ecc94f953dbd1a4ca02ea7c8b6db28cca3370
Guilherme Amoroso Romão <guilherme.romao@mirriad.com>
2024-06-03 22:57:48 +0100

    Fixed activation URL

--- src/services/AccountConfirmationService.ts
+++ src/services/AccountConfirmationService.ts
@@ -2,10 +2,9 @@ import { SERVICE_URL } from "../Constants";
 import axios, { AxiosResponse } from "axios";
 
 export class AccountConfirmationService {
-    CONFIRMATION_BASE_URL = `${SERVICE_URL}/users/activate`;
 
     async confirmAccount(userId: string, confirmationId: string): Promise<AxiosResponse> {
-        const confirmationUrl = `${this.CONFIRMATION_BASE_URL}/${userId}/${confirmationId}`;
+        const confirmationUrl = `users/${userId}/activate/${confirmationId}`;
 
         return await axios.get(confirmationUrl);
     }
