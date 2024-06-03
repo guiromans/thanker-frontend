7cc8acab09b2f92c2f06dd980f01efc2e3479260
Guilherme Amoroso Romão <guilherme.romao@mirriad.com>
2024-06-03 22:58:28 +0100

    Code polish

--- src/services/AccountConfirmationService.ts
+++ src/services/AccountConfirmationService.ts
@@ -1,10 +1,9 @@
-import { SERVICE_URL } from "../Constants";
 import axios, { AxiosResponse } from "axios";
 
 export class AccountConfirmationService {
 
     async confirmAccount(userId: string, confirmationId: string): Promise<AxiosResponse> {
-        const confirmationUrl = `users/${userId}/activate/${confirmationId}`;
+        const confirmationUrl = `/users/${userId}/activate/${confirmationId}`;
 
         return await axios.get(confirmationUrl);
     }
