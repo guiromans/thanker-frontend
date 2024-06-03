565d2133ce4e90a5357e341c471b74bbe57b3a9f
Guilherme Amoroso Romão <guilherme.romao@mirriad.com>
2024-06-03 23:04:56 +0100

    Updated confirmation URL

--- src/services/AccountConfirmationService.ts
+++ src/services/AccountConfirmationService.ts
@@ -1,9 +1,10 @@
+import { SERVICE_URL } from "../Constants";
 import axios, { AxiosResponse } from "axios";
 
 export class AccountConfirmationService {
-
+    
     async confirmAccount(userId: string, confirmationId: string): Promise<AxiosResponse> {
-        const confirmationUrl = `/users/${userId}/activate/${confirmationId}`;
+        const confirmationUrl = `${SERVICE_URL}/users/${userId}/activate/${confirmationId}`;
 
         return await axios.get(confirmationUrl);
     }
