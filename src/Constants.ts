3da8ff258e5df3f03aa4ef0d6b7ad5087d59c6cf
Guilherme Amoroso Romão <guilherme.romao@mirriad.com>
2024-09-14 18:56:29 +0100

    Updated API URL

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
 const LOCAL_URL: string = `http://localhost:8095`;
 const CLOUD_URL: string = `https://api.thanker.co:8443`;
 
-export const SERVICE_URL = LOCAL_URL;
+export const SERVICE_URL = CLOUD_URL;
